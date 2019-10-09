9f8d0626bc2e4ceff4864b8c42826b0ce8e65240
Artyom Afonin <joniclife@gmail.com>
2019-10-09 23:52:36 +0700

    Change MainWindow import path

--- src/core/GraphicalEditor.java
+++ src/core/GraphicalEditor.java
@@ -1,12 +1,12 @@
 package core;
 
-import core.App;
+import core.scene.MainWindow;
 import javafx.application.Application;
 
 import static javafx.application.Application.launch;
 
 public class GraphicalEditor {
     public static void main(String[] args) {
-        Application.launch(App.class, args);
+        Application.launch(MainWindow.class, args);
     }
 }
