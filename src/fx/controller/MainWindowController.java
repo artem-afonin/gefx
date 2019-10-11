1c67082a31c230ef6afce16071ca3c0c304bf362
Artyom Afonin <joniclife@gmail.com>
2019-10-11 20:16:13 +0700

    Implement drawing

--- src/fx/controller/MainWindowController.java
+++ src/fx/controller/MainWindowController.java
@@ -1,10 +1,27 @@
 package fx.controller;
 
 import core.scene.AboutWindow;
+import core.tool.DotTool;
+import core.tool.DrawHandler;
+import core.tool.DrawTool;
+import core.tool.LineTool;
 import javafx.event.ActionEvent;
+import javafx.scene.Group;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 public class MainWindowController {
+    public Canvas canvas;
+    public Group drawGroup;
+    public Pane drawPane;
+
+    public void initialize() {
+        DrawTool tool = new DotTool();
+        DrawHandler drawHandler = new DrawHandler(canvas, tool);
+        drawHandler.runHandler();
+    }
+
     public void closeApplication(ActionEvent actionEvent) {
         System.exit(0);
     }
