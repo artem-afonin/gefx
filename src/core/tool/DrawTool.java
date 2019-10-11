7203e6f762342ab7f78e9f1d6c94969fe1cb0a11
Artyom Afonin <joniclife@gmail.com>
2019-10-11 18:50:22 +0700

    Expand DrawTool class

--- src/core/tool/DrawTool.java
+++ src/core/tool/DrawTool.java
@@ -2,9 +2,13 @@ package core.tool;
 
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.paint.Color;
+import javafx.scene.shape.StrokeLineCap;
 
 abstract public class DrawTool {
     protected Color color;
+    protected double currentOpacity;
+    protected double strokeWidth;
+    protected StrokeLineCap strokeLineCap;
     protected GraphicsContext graphicsContext;
 
     public DrawTool(GraphicsContext context) {
@@ -14,6 +18,9 @@ abstract public class DrawTool {
     public DrawTool(GraphicsContext context, Color color) {
         this.graphicsContext = context;
         this.color = color;
+        currentOpacity = 1;
+        strokeWidth = 5;
+        strokeLineCap = StrokeLineCap.ROUND;
     }
 
     public Color getColor() {
@@ -24,11 +31,41 @@ abstract public class DrawTool {
         this.color = color;
     }
 
+    public double getCurrentOpacity() {
+        return currentOpacity;
+    }
+
+    public void setCurrentOpacity(double currentOpacity) {
+        this.currentOpacity = currentOpacity;
+    }
+
+    public double getStrokeWidth() {
+        return strokeWidth;
+    }
+
+    public void setStrokeWidth(double strokeWidth) {
+        this.strokeWidth = strokeWidth;
+    }
+
+    public StrokeLineCap getStrokeLineCap() {
+        return strokeLineCap;
+    }
+
+    public void setStrokeLineCap(StrokeLineCap strokeLineCap) {
+        this.strokeLineCap = strokeLineCap;
+    }
+
     public GraphicsContext getGraphicsContext() {
         return graphicsContext;
     }
 
     public void setGraphicsContext(GraphicsContext graphicsContext) {
         this.graphicsContext = graphicsContext;
+    }
+
+    protected void configureGraphicsContext() {
+        graphicsContext.setStroke(color);
+        graphicsContext.setLineWidth(strokeWidth);
+        graphicsContext.setLineCap(strokeLineCap);
     }
 }
