49c088ad415e73b885721cc1c2b1f559215f2a1b
Artyom Afonin <joniclife@gmail.com>
2019-10-15 20:56:39 +0700

    Add tool methods

--- src/gefx/controller/MainWindowController.java
+++ src/gefx/controller/MainWindowController.java
@@ -4,10 +4,12 @@ import gefx.core.scene.AboutWindow;
 import gefx.core.tool.DotTool;
 import gefx.core.tool.DrawHandler;
 import gefx.core.tool.DrawTool;
+import gefx.core.tool.LineTool;
 import javafx.event.ActionEvent;
 import javafx.scene.Group;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.layout.Pane;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 
 public class MainWindowController {
@@ -15,9 +17,17 @@ public class MainWindowController {
     public Group drawGroup;
     public Pane drawPane;
 
+    private DrawTool tool;
+    private Color toolColor;
+    private double toolWidth;
+    private double toolOpacity;
+
+    private DrawHandler drawHandler;
+
+
     public void initialize() {
-        DrawTool tool = new DotTool();
-        DrawHandler drawHandler = new DrawHandler(canvas, tool);
+        setTool(new DotTool());
+        drawHandler = new DrawHandler(canvas, tool);
         drawHandler.runHandler();
     }
 
@@ -33,5 +43,20 @@ public class MainWindowController {
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    public void setTool(DrawTool tool) {
+        this.tool = tool;
+        this.tool.setColor(toolColor);
+        this.tool.setStrokeWidth(toolWidth);
+        this.tool.setCurrentOpacity(toolOpacity);
+    }
+
+    public void setDotTool() {
+        setTool(new DotTool());
+    }
+
+    public void setLineTool() {
+        setTool(new LineTool());
     }
 }
