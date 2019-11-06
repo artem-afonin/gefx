5d030edd648db49fce4fc79369ea38d5949d0c50
Artyom Afonin <joniclife@gmail.com>
2019-11-06 10:29:06 +0700

    Update AboutWindow

--- src/gefx/controller/AboutWindowController.java
+++ src/gefx/controller/AboutWindowController.java
@@ -2,9 +2,10 @@ package gefx.controller;
 
 import javafx.event.ActionEvent;
 import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.GridPane;
 
 public class AboutWindowController {
-    public AnchorPane windowPane;
+    public GridPane windowPane;
 
     public void closeWindow(ActionEvent actionEvent) {
         windowPane.getScene().getWindow().hide();
